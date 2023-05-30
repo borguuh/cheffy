51e51fdcad17d375ad4da6f9a199a7fa446c87dd
Borguuh <abdrahmanadewale@gmail.com>
2023-05-30 22:48:07 +0100

    [feat] unsuscribe from newsletter

--- controllers/User/newsletter_controller.js
+++ controllers/User/newsletter_controller.js
@@ -20,3 +20,30 @@ exports.subscribeToNewsletter = async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+exports.unsubscribeNewsletter = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Check if the email exists in the newsletter subscription list
+    const existingEmail = await NewsletterEmail.findOne({ email });
+
+    if (!existingEmail) {
+      return res
+        .status(404)
+        .json({
+          message: "Email not found in the newsletter subscription list.",
+        });
+    }
+
+    // Remove the email from the newsletter subscription list
+    await NewsletterEmail.deleteOne({ email });
+
+    return res
+      .status(200)
+      .json({ message: "Successfully unsubscribed from the newsletter." });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- routes/User/newsletter_route.js
+++ routes/User/newsletter_route.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {
   subscribeToNewsletter,
+  unsubscribeNewsletter,
 } = require("../../controllers/User/newsletter_controller");
 
 router.post("/newsletter/subscribe", subscribeToNewsletter);
+router.post("/newsletter/unsubscribe", unsubscribeNewsletter);
 
 module.exports = router;
